01e60a28b8f1452b9a0e59dba4ce7980fb8d6da6
Kais Dukes <dukeskais79@gmail.com>
2020-08-13 23:05:01 +0100

    Odd occurrences in an array.

---
+++ problems/src/main/java/problems/codility/OddOccurrencesInArray.java
@@ -0,0 +1,31 @@
+package problems.codility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OddOccurrencesInArray {
+
+    // Assuming that all but one of the values in A occurs an even number of times,
+    // find the unique value of element that occurs an odd number of times.
+    public static int oddOccurrenceInArray(final int[] array) {
+
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Array should not be null or empty.");
+        }
+
+        final Map<Integer, Integer> counts = new HashMap<>();
+        for (final int element : array) {
+            final Integer count = counts.get(element);
+            counts.put(element, count != null ? count + 1 : 1);
+        }
+
+        for (final Map.Entry<Integer, Integer> pair : counts.entrySet()) {
+            if (pair.getValue() % 2 == 1) {
+                return pair.getKey();
+            }
+        }
+
+        throw new IllegalArgumentException(
+                "Array does not contain an element that occurs an odd number of times.");
+    }
+}
---
+++ problems/src/test/java/problems/codility/TestOddOccurrencesInArray.java
@@ -0,0 +1,49 @@
+package problems.codility;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static problems.codility.OddOccurrencesInArray.oddOccurrenceInArray;
+
+public class TestOddOccurrencesInArray {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldGetOddOccurrence() {
+
+        assertThat(
+                oddOccurrenceInArray(new int[]{9, 3, 9, 3, 9, 7, 9}),
+                is(equalTo(7)));
+    }
+
+    @Test
+    public void shouldThrowForNullArray() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Array should not be null or empty.");
+
+        oddOccurrenceInArray(null);
+    }
+
+    @Test
+    public void shouldThrowForEmptyArray() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Array should not be null or empty.");
+
+        oddOccurrenceInArray(new int[0]);
+    }
+
+    @Test
+    public void shouldThrowForArrayWithoutOddOccurrence() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(
+                "Array does not contain an element that occurs an odd number of times.");
+
+        oddOccurrenceInArray(new int[]{9, 3, 9, 3, 9, 7, 7, 9});
+    }
+}
